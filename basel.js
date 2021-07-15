d769683c9797d21f5fe8011956dce1d7c98e1696
babosalah <77887791+babosalah@users.noreply.github.com>
2021-07-15 10:20:05 +0200

    save

--- basel.js
+++ basel.js
@@ -1,5 +1,5 @@
 
-$(document).ready(function(){
+/*$(document).ready(function(){
 
     $(".card").mouseover(
         function(){
@@ -10,8 +10,7 @@ $(document).ready(function(){
     
         });
     });
-
-
+*/
 $(function(){
 
    
@@ -27,25 +26,70 @@ $(function(){
      $('html').niceScroll({
     
         cursorcolor: '#051e34' ,
-        cursorwidth: '9px',
-        corsorborder: '1px solid #d31336',
+        cursorwidth: '10px',
+     
         corsorborderradius:0,
         zindex:99999
     
      });
-     //////////////////
+     
+    //smooth scroll
+    //1
+    $('.resume').click(function(){
+    
+        $('html , body').animate({
+    
+            scrollTop: $('.about-me').offset().top
+    
+        },1000);
+    
+    });
+    //2
+    $('.services').click(function(){
+    
+        $('html , body').animate({
+    
+            scrollTop: $('.service').offset().top
+    
+        },1500);
+    
+    });
+//3
 
-     $(".services").click({
 
-    function () {
-        $(".card").hidde();
-        
-    } })
+    $('.pricing').click(function(){
+    
+        $('html , body').animate({
+    
+            scrollTop: $('.pricing-plans').offset().top
+    
+        },1500);
+    
+    });
+//4
+$('.blog').click(function(){
+    
+    $('html , body').animate({
 
+        scrollTop: $('.latest-post').offset().top
+
+    },1500);
+
+});
+
+
+//5
+    $('.contact').click(function(){
+    
+        $('html , body').animate({
+    
+            scrollTop: $('.cont').offset().top
+    
+        },2000);
     
     });
 
-
+});
 
 
       
